0bdab7bf40958432b112943781869588463bb0d1
Aljun04 <57779846+Aljun04@users.noreply.github.com>
2019-11-20 17:33:41 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<center>
+	<title>Hello World</title>
+</head>
+<body>
+<h1>
+	<?php  echo "Hello,  World!";?>
+	</h1>
+	<a href="welcome.php"><button>home</button> </a>
+	</center>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<center>
+</head>
+<body>
+<h1>
+	<?php  echo"  Aljun M." ;?>
+	</h1>
+	<?php //this is comment only //?>
+	<a href="index.php"><button>back</button></a>
+</center>
+</body>
+</html>
