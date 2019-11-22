68d2f54bfd0a8b48e3b8d21c37efd1bfc4c89b6c
Aljun04 <57779846+Aljun04@users.noreply.github.com>
2019-11-21 17:24:10 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
 	<title>Hello World</title>
 </head>
 <body>
+<div style="background-image:url(pic.png); border-radius: 12px;  border: 5px solid black; text-decoration: none; height: 500px; width: 500px">
 <h1>
+
 	<?php  echo "Hello,  World!";?>
 	</h1>
-	<a href="welcome.php"><button>home</button> </a>
+	</div>
+	<a href="welcome.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;">home</button></a>
 	</center>
 </body>
 </html>
