bf023e5b6698778a7404e3ef8724d7940bfa113e
Aljun04 <57779846+Aljun04@users.noreply.github.com>
2019-11-21 18:12:27 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -10,9 +10,9 @@
 	</h1>
 	<div style="background-color:yellow; border-radius:12px; border: 5px solid black; text-decoration: none; height: 500px; width: 500px;">
 	<?php //this is comment only //?>
-	<br><br><br><a href="welcome.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Welcome</button> </a><br>
+	<br><br><br><a href="displaymethod.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Welcome</button> </a><br>
 	<a href="getmethod.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Getmethod</button> </a><br>
-	<a href="displaymethod.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Displaymethod</button> </a><br>
+	<a href="Welcome.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Displaymethod</button> </a><br>
 	<a href="post.php"><button style="border-radius: 5px; border-width: 5px; font-size: 15px;" >Post</button> </a><br>
 
 
