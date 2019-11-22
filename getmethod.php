7b03e5503cf042a2702558fd78ab5afc334e0f75
Aljun04 <57779846+Aljun04@users.noreply.github.com>
2019-11-21 18:24:39 -0800

    Update getmethod.php

--- getmethod.php
+++ getmethod.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,9 +16,9 @@
 <br>
 <br>
 <form action="displaymethod" method = "GET">
-  Name:<input type="text" name="lastname"/><br>
-  Age:<input type="text" name="Age"/><br>
-  Password:<input type="password" name="pass"/><br>
+  Name:<input style="margin-left: 25px" type="text" name="lastname"/><br>
+  Age:<input style="margin-left: 35px" type="text" name="Age"/><br>
+  Password:<input style="margin-left: 1px" type="password" name="pass"/><br>
 
   <input style="border-radius: 5px; border-width: 5px; font-size: 15px;" type="submit" value="login">
   </div>
